764765ee083aeab30567e1e151024e40d2614eb3
akimatter <akm2000@gmail.com>
2009-10-29 08:28:47 +0900

    gem化に伴いinit.rbをシンプルにしました。READMEにgemで使う場合の記述を追加しました。

--- init.rb
+++ init.rb
@@ -1,33 +1,4 @@
 unless ENV['SCHEMA_COMMENTS_DISABLED']
-
   require 'schema_comments'
-
-  base_names = %w(Base Migration Migrator Schema SchemaDumper) +
-    %w(Column ColumnDefinition TableDefinition).map{|name| "ConnectionAdapters::#{name}"}
-
-  base_names.each do |base_name|
-    ar_class = "ActiveRecord::#{base_name}".constantize
-    sc_class = "SchemaComments::#{base_name}".constantize
-    unless ar_class.ancestors.include?(sc_class)
-      ar_class.__send__(:include, sc_class)
-    end
-  end
-
-  unless ActiveRecord::ConnectionAdapters::AbstractAdapter.ancestors.include?(SchemaComments::ConnectionAdapters::Adapter)
-    class ActiveRecord::ConnectionAdapters::AbstractAdapter
-      include SchemaComments::ConnectionAdapters::Adapter
-    end
-  end
-
-  # %w(Mysql PostgreSQL SQLite3 SQLite Firebird DB2 Oracle Sybase Openbase Frontbase)
-  %w(Mysql PostgreSQL SQLite3 SQLite).each do |adapter|
-    begin
-      require("active_record/connection_adapters/#{adapter.downcase}_adapter")
-      adapter_class = ('ActiveRecord::ConnectionAdapters::' << "#{adapter}Adapter").constantize
-      adapter_class.module_eval do
-        include SchemaComments::ConnectionAdapters::ConcreteAdapter
-      end
-    rescue Exception => e
-    end
-  end
+  SchemaComments.setup
 end
--- lib/schema_comments.rb
+++ lib/schema_comments.rb
@@ -1,5 +1,5 @@
 module SchemaComments
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
   
   autoload :Base, 'schema_comments/base'
   autoload :ConnectionAdapters, 'schema_comments/connection_adapters'
@@ -16,4 +16,38 @@ module SchemaComments
 
   mattr_accessor :quiet
   
+  class << self
+    def setup
+      base_names = %w(Base Migration Migrator Schema SchemaDumper) +
+        %w(Column ColumnDefinition TableDefinition).map{|name| "ConnectionAdapters::#{name}"}
+      
+      base_names.each do |base_name|
+        ar_class = "ActiveRecord::#{base_name}".constantize
+        sc_class = "SchemaComments::#{base_name}".constantize
+        unless ar_class.ancestors.include?(sc_class)
+          ar_class.__send__(:include, sc_class)
+        end
+      end
+      
+      unless ActiveRecord::ConnectionAdapters::AbstractAdapter.ancestors.include?(SchemaComments::ConnectionAdapters::Adapter)
+        ActiveRecord::ConnectionAdapters::AbstractAdapter.module_eval do
+          include SchemaComments::ConnectionAdapters::Adapter
+        end
+      end
+      
+      # %w(Mysql PostgreSQL SQLite3 SQLite Firebird DB2 Oracle Sybase Openbase Frontbase)
+      %w(Mysql PostgreSQL SQLite3 SQLite).each do |adapter|
+        begin
+          require("active_record/connection_adapters/#{adapter.downcase}_adapter")
+          adapter_class = ('ActiveRecord::ConnectionAdapters::' << "#{adapter}Adapter").constantize
+          adapter_class.module_eval do
+            include SchemaComments::ConnectionAdapters::ConcreteAdapter
+          end
+        rescue Exception => e
+        end
+      end
+    end
+    
+  end
+
 end
