71e897447cef34de834f83331d6876ee54be7a30
akm <akm2000@gmail.com>
2016-06-30 07:59:28 +0900

    Remove rails/test_help not to call Minitest.autorun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ ENV["DB"] ||= "mysql"
 
 require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
-require "rails/test_help"
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
