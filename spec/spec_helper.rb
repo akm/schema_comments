a6f1b6ab877ce107737613c3d9872a140dd3d513
akm <akm2000@gmail.com>
2016-06-29 16:29:26 +0900

    Add spec_helper.rb before to add specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require "simplecov"
+SimpleCov.start
+
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
+require "rails/test_help"
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
