f2df6c4842f0bc0aae82230a858d9d91729281bc
akm <akm2000@gmail.com>
2016-06-30 06:59:18 +0900

    Fix the path to dummy app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ ENV["RAILS_ENV"] = "test"
 # Configure Rails Environment
 ENV["DB"] ||= "mysql"
 
-require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
+require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
 
 # Load support files
