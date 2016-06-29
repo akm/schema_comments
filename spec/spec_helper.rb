581eab2286ce487b0fb2905be17ad11c2bca5d6d
akm <akm2000@gmail.com>
2016-06-30 08:12:15 +0900

    Fix connection to slite3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.start
 ENV["RAILS_ENV"] = "test"
 
 # Configure Rails Environment
-ENV["DB"] ||= "mysql"
+ENV["DB"] ||= 'sqlite3' # "mysql2"
 
 require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
