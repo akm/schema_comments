74458ed53c70a4a1c9eb30d50e29c5a400215c3a
akm <akm2000@gmail.com>
2016-07-04 23:23:56 +0900

    Use dummy4rail5 for activerecord-5.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,12 @@ ENV["RAILS_ENV"] = "test"
 # Configure Rails Environment
 ENV["DB"] ||= 'sqlite3' # "mysql2"
 
-require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
+dummy_path =
+  (Gem.loaded_specs['rails'].version >= Gem::Version.new('5.0.0')) ?
+    'spec/dummy4rails5' : 'spec/dummy'
+
+require File.expand_path("../../#{dummy_path}/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../#{dummy_path}/db/migrate", __FILE__)]
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
