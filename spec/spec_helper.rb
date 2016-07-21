53939298d8b39f897f7c0980bb0d227401cbfbb8
akm <akm2000@gmail.com>
2016-07-21 19:09:33 +0900

    DB=mysqlでうまく動かなかったので修正しました

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,14 +9,16 @@ ENV["RAILS_ENV"] = "test"
 # Configure Rails Environment
 ENV["DB"] ||= 'sqlite3' # "mysql2"
 
-require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../dummy/db/migrate", __FILE__)]
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 SchemaComments.setup
 
-Dir.chdir(File.expand_path("../../spec/dummy",  __FILE__)) do
-  system('RAILS_ENV=test bin/rake db:create')
-end
+# Dir.chdir(File.expand_path("../dummy",  __FILE__)) do
+#   system('RAILS_ENV=test bin/rake db:create')
+# end
+
+ActiveRecord::Tasks::DatabaseTasks.create_current
