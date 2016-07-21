d9da6aa09b198f1f6369f168d3d6470a92e99bb8
akm <akm2000@gmail.com>
2016-07-21 18:41:10 +0900

    Call setup before rake task starts

--- lib/schema_comments/railtie.rb
+++ lib/schema_comments/railtie.rb
@@ -6,6 +6,7 @@ module SchemaScomments
   class Railtie < ::Rails::Railtie
     rake_tasks do
       SchemaComments.yaml_path = Rails.root.join("db/schema_comments.yml").to_s
+      SchemaComments.setup
       require 'schema_comments/task'
     end
   end
